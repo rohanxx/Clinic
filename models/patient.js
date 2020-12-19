ad7bd198fdde6e279288c1bb3aed731e17441b35
abhi <52235699+stagnet@users.noreply.github.com>
2020-12-19 17:42:59 +0530

    added fields

--- models/patient.js
+++ models/patient.js
@@ -4,6 +4,29 @@ const Joi = require("joi");
 const Patient = mongoose.model(
   "Patient",
   new mongoose.Schema({
+    address: {
+      type: String,
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    diagnosis: {
+      type: String,
+      required: true,
+    },
+    digitalSign: {
+      type: String,
+    },
     email: {
       type: String,
       required: true,
@@ -11,20 +34,17 @@ const Patient = mongoose.model(
       maxlength: 255,
       unique: true,
     },
-    dob: {
-      type: Date,
-      required: true,
-    },
-    description: {
+    ergonomic_advice: {
       type: String,
       required: true,
     },
-    address: {
+    fee: {
       type: String,
       required: true,
     },
-    bloodGroup: {
+    gender: {
       type: String,
+      enum: ["Male", "Female", "other"],
       required: true,
     },
     password: {
@@ -37,18 +57,37 @@ const Patient = mongoose.model(
       type: String,
       required: true,
     },
+    referred_by_dr: {
+      type: String,
+      default: "",
+    },
+    treatment: {
+      type: String,
+      required: true,
+    },
+    weight: {
+      type: String,
+      required: true,
+    },
   })
 );
 
 function validate(patient) {
   const schema = Joi.object({
     appointmentId: Joi.string().required(),
-    email: Joi.string().min(5).max(255).required().email(),
-    dob: Joi.string().required(),
-    description: Joi.string().required(),
     address: Joi.string().required(),
     bloodGroup: Joi.string().required(),
+    dob: Joi.string().required(),
+    description: Joi.string().required(),
+    diagnosis: Joi.string().max(255),
+    email: Joi.string().min(5).max(255).required().email(),
+    ergonomic_advice: Joi.string(),
+    fee: Joi.string().required(),
+    gender: Joi.string().required(),
     password: Joi.string().min(5).max(255).required(),
+    referred_by_dr: Joi.string(),
+    treatment: Joi.string().required(),
+    weight: Joi.string(),
   });
 
   return schema.validate(patient);
